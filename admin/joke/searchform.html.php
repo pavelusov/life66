f49107f96e4fa23725c032cdec81ebfe9d1fad6d
pavelusov <ipavelusov@gmail.com>
2016-03-30 13:39:39 +0500

    Пишем вьювер. Добавили Первый раскрывающийся список

--- admin/joke/searchform.html.php
+++ admin/joke/searchform.html.php
@@ -1,3 +1,6 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/func.inc.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +8,19 @@
     <title>Управление поговороками</title>
 </head>
 <body>
-
+    <h1>Управление поговороками</h1>
+    <p><a href="?add">Добавить новую поговорку</a></p>
+    <form action="" method="get">
+        <p>Критерии поговорок: </p>
+        <div>
+            <label for="author">По автору:</label>
+            <select name="author" >
+                <option value="">Любой автор</option>
+                <?php foreach ($authors as $author):?>
+                <option value="<?php htmlout($author['id']);?>"><?php htmlout($author['name']);?></option>
+                <?php endforeach;?>
+            </select>
+        </div>
+    </form>
 </body>
 </html>
