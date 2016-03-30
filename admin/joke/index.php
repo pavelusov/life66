77f94ef2b526faa8fc89fe9c3f8f3039afac5b46
pavelusov <ipavelusov@gmail.com>
2016-03-30 13:23:58 +0500

    Создали контроллер управления поговорками

---
+++ admin/joke/index.php
@@ -0,0 +1,31 @@
+<?php
+include_once 'connect.php';
+//Получаем список авторов
+try{
+    $result = $pdo->query('SELECT `id`, `name` FROM `joke_users`');
+}catch(PDOException $err){
+    $error = 'Ошибка при получении списка авторов' . $err->getMessage();
+    include $_SERVER['DOCUMENT_ROOT'] . '/error.html.php';
+    exit;
+}
+foreach ($result as $item) {
+    $authors[] = [
+        'id'=>$item['id'],
+        'name'=>$item['name']
+    ];
+}
+//Получаем список категорий
+try{
+    $result = $pdo->query('SELECT `id`, `cat_name` FROM `category`');
+}catch(PDOException $err){
+    $error = 'Ошибка при получении списка категорий' . $err->getMessage();
+    include $_SERVER['DOCUMENT_ROOT'] . '/error.html.php';
+    exit;
+}
+foreach ($result as $item) {
+    $category[] = [
+        'id'=>$item['id'],
+        'name'=>$item['cat_name']
+    ];
+}
+include 'searchform.html.php';
---
+++ admin/joke/searchform.html.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Управление поговороками</title>
+</head>
+<body>
+
+</body>
+</html>
